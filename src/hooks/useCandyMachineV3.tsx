bdfcbdf548a986a74b43333ff8fd1aa6e58ac173
timov <timovmartin@gmail.com>
2023-06-20 11:57:51 +0200

    merle prit

--- src/hooks/useCandyMachineV3.tsx
+++ src/hooks/useCandyMachineV3.tsx
@@ -116,6 +116,8 @@ export default function useCandyMachineV3(
       let merkle = merkles[label];
       if (!merkle) return;
       console.log("MERKLE!");
+      let hex = Buffer.from(merkle).toString('hex');
+      console.log(hex);
       const verifiedProof = !!merkle.proof.length;
       const compareRoot = merkle.tree.getRoot().equals(Buffer.from(merkleRoot));
       console.log(verifiedProof);
